1d5cd32312ad914d49ed140b459b726d1881bb59
devon <devonjhills@gmail.com>
2021-08-22 17:18:11 -0400

    create base components and navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import "./App.css";
+import Home from "./components/Home";
+import Leaderboard from "./components/Leaderboard";
+import Login from "./components/Login";
+import Nav from "./components/Nav";
+import NewQuestion from "./components/NewQuestion";
 
 function App() {
   return (
-    <div className="App">
-    Would You Rather
-    </div>
+    <Router>
+      <Fragment>
+        <div className="container">
+          <Nav />
+          <div>
+            <Route path="/" exact component={Home} />
+            <Route path="/add" component={NewQuestion} />
+            <Route path="/leaderboard" component={Leaderboard} />
+            <Route path="/login" component={Login} />
+          </div>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div>
+      <nav className="nav">
+        <ul>
+          <li>
+            <NavLink to="/" exact activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/add" activeClassName="active">
+              New Question
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/leaderboard" activeClassName="active">
+              Leaderboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" activeClassName="active">
+              Logout
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;

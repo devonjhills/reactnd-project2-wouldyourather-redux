0e5b2303125256def53ce79a4a3806311f941f07
devon <devonjhills@gmail.com>
2021-08-22 20:27:18 -0400

    add base redux reducers

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,6 +1,6 @@
 import { SET_AUTHED_USER } from "../actions/authedUser";
 
-const authedUser = (state = "", action) => {
+const authedUser = (state = null, action) => {
   switch (action.type) {
     case SET_AUTHED_USER:
       return action.id;
